62050dd390793015334afaa1ce519cfbf9c1d509
NguyenAnhTu <anhtu8143@gmail.com>
2024-02-29 14:11:34 +0700

    second cm

---
+++ Student.java
@@ -0,0 +1,19 @@
+import java.util.AbstractQueue;
+
+public class Student {
+    private String id;
+
+    private String name;
+
+    private String add;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+
+}
